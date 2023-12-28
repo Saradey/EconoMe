7e3144dedc0500cff4ba6c9e6717c5e289525db4
Evgeny Goncharov <jtgn@yandex.ru>
2023-12-28 18:08:46 +0200

    fix detekt

--- app/src/test/java/evgenii/goncharov/econome/ExampleUnitTest.kt
+++ app/src/test/java/evgenii/goncharov/econome/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package evgenii.goncharov.econome
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
