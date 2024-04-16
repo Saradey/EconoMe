232917392248cb1727599bbe8b63ee81a69d6378
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-16 19:00:34 +0300

    added code

--- sources/engine/navigation/build.gradle.kts
+++ sources/engine/navigation/build.gradle.kts
@@ -1,29 +1,9 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
-plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
-}
+plugins {}
 
 android {
     namespace = "evgenii.goncharov.econome.navigation"
-    compileSdk = extra["compileSdk"].toString().toInt()
-    defaultConfig {
-        minSdk = extra["minSdk"].toString().toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = extra["kotlinTarget"] as String
-    }
 }
 
 dependencies {
 
-    implementation(libs.core.ktx)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
