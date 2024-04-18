e95ebb6af89cd8596b2489976b93753dcf640b35
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-18 14:58:51 +0300

    added code

---
+++ app/src/main/kotlin/evgenii/goncharov/econome/di/modules/FeatureHoldersModule.kt
@@ -0,0 +1,23 @@
+package evgenii.goncharov.econome.di.modules
+
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.ClassKey
+import dagger.multibindings.IntoMap
+import evgenii.goncharov.econome.di_core.containers.FeatureContainer
+import evgenii.goncharov.econome.di_core.contracts.ReleasableApi
+import evgenii.goncharov.econome.di_core.holders.FeatureHolder
+import javax.inject.Singleton
+
+@Module
+object FeatureHoldersModule {
+
+    @[Singleton Provides]
+    @[IntoMap ClassKey(ReleasableApi::class)]
+    fun provideMainActivityHolder(featureContainer: FeatureContainer): FeatureHolder<*> =
+        object : FeatureHolder<ReleasableApi>(featureContainer) {
+            override fun buildComponent(): ReleasableApi {
+                return object : ReleasableApi {}
+            }
+        }
+}
--- app/src/main/kotlin/evgenii/goncharov/econome/initializer/FeatureHolderInitializerImpl.kt
+++ app/src/main/kotlin/evgenii/goncharov/econome/initializer/FeatureHolderInitializerImpl.kt
@@ -1,5 +1,6 @@
 package evgenii.goncharov.econome.initializer
 
+import evgenii.goncharov.econome.di.DaggerFeatureHoldersComponent
 import evgenii.goncharov.econome.di_core.containers.FeatureContainerManager
 import evgenii.goncharov.econome.di_core.holders.FeatureHolder
 import evgenii.goncharov.econome.di_core.initializer.FeatureHolderInitializer
@@ -7,6 +8,8 @@ import evgenii.goncharov.econome.di_core.initializer.FeatureHolderInitializer
 class FeatureHolderInitializerImpl : FeatureHolderInitializer {
 
     override fun init(featureContainerManager: FeatureContainerManager): Map<Class<*>, FeatureHolder<*>> {
-        TODO("Not yet implemented")
+        return DaggerFeatureHoldersComponent.factory().create(
+            featureContainerManager
+        ).getFeatureHolders()
     }
 }
