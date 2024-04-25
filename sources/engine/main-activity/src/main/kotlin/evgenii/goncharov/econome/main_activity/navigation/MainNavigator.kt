01308dbf829a4c2eb362dd5aee1e299a6769bd2f
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 16:10:38 +0300

    added code

--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/navigation/MainNavigator.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/navigation/MainNavigator.kt
@@ -4,6 +4,9 @@ import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.FragmentManager
 import com.github.terrakok.cicerone.Command
+import com.github.terrakok.cicerone.Forward
+import com.github.terrakok.cicerone.Replace
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 import evgenii.goncharov.econome.navigation.base.BaseNavigator
 import evgenii.goncharov.econome.main_activity.R
 
@@ -16,6 +19,30 @@ internal class MainNavigator(
     override val containerId: Int = R.id.container
 
     override fun applyCommand(command: Command) {
+        when (command) {
+            is Forward -> forward(command)
+            is Replace -> replace(command)
+        }
+    }
 
+    private fun forward(command: Forward) {
+        val fragmentScreen = command.screen as FragmentScreen
+        val featureContainerFragment = fragmentScreen.createFragment(ff)
+        commitFragmentTransaction(
+            fragment = featureContainerFragment,
+            fragmentScreen = fragmentScreen,
+            backStackName = fragmentScreen.screenKey
+        )
+    }
+
+    private fun replace(command: Replace) {
+        fm.popBackStack()
+        val fragmentScreen = command.screen as FragmentScreen
+        val featureContainerFragment = fragmentScreen.createFragment(ff)
+        commitFragmentTransaction(
+            fragment = featureContainerFragment,
+            fragmentScreen = fragmentScreen,
+            backStackName = fragmentScreen.screenKey
+        )
     }
 }
