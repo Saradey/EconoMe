da145115a95889a847ba4bcc9be309dfa358c94c
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 16:43:15 +0300

    added code

--- app/src/main/kotlin/evgenii/goncharov/econome/di/modules/FeatureHoldersModule.kt
+++ app/src/main/kotlin/evgenii/goncharov/econome/di/modules/FeatureHoldersModule.kt
@@ -7,11 +7,16 @@ import dagger.multibindings.IntoMap
 import evgenii.goncharov.econome.main_activity.di.contracts.MainActivityApi
 import evgenii.goncharov.econome.main_activity.di.holder.MainActivityHolder
 import evgenii.goncharov.econome.di_core.holders.FeatureHolder
+import evgenii.goncharov.econome.user_api.di.UserCreatorApi
+import evgenii.goncharov.econome.user_impl.di.holder.UserCreatorHolder
 import javax.inject.Singleton
 
 @Module
 interface FeatureHoldersModule {
 
     @[Binds Singleton IntoMap ClassKey(MainActivityApi::class)]
-    fun provideMainActivityHolder(mainActivityHolder: MainActivityHolder): FeatureHolder<*>
+    fun bindMainActivityHolder(mainActivityHolder: MainActivityHolder): FeatureHolder<*>
+
+    @[Binds Singleton IntoMap ClassKey(UserCreatorApi::class)]
+    fun bindUserCreatorHolder(mainActivityHolder: UserCreatorHolder): FeatureHolder<*>
 }
