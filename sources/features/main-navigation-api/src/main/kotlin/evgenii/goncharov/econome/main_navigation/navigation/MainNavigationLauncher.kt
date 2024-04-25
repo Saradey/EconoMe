7eedf36e844df38464fbac7062adfcee426535a8
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 17:40:06 +0300

    added code

---
+++ sources/features/main-navigation-api/src/main/kotlin/evgenii/goncharov/econome/main_navigation/navigation/MainNavigationLauncher.kt
@@ -0,0 +1,8 @@
+package evgenii.goncharov.econome.main_navigation.navigation
+
+import evgenii.goncharov.econome.di_core.contracts.BaseLauncher
+
+public interface MainNavigationLauncher : BaseLauncher {
+
+    public fun launchMainNavigation()
+}
