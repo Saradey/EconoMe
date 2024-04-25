cd4845154e1ef2be223354d040d301a61458c3a2
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 16:15:10 +0300

    added code

--- sources/features/user-impl/src/main/kotlin/evgenii/goncharov/econome/user_impl/fragments/UserCreatorFragment.kt
+++ sources/features/user-impl/src/main/kotlin/evgenii/goncharov/econome/user_impl/fragments/UserCreatorFragment.kt
@@ -1,7 +1,12 @@
 package evgenii.goncharov.econome.user_impl.fragments
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import evgenii.goncharov.econome.di_core.CoreFragment
 
 /**
@@ -11,9 +16,17 @@ internal class UserCreatorFragment : CoreFragment() {
 
     @Composable
     override fun InitContent() {
-        Text(text = "UserCreatorFragment Создание пользователя")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = "UserCreatorFragment Создание пользователя",
+                color = Color.White,
+                fontSize = 20.sp
+            )
+        }
     }
-
 
     companion object {
 
