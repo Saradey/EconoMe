b4393216d0c672cc213a7820a4a2fde45d61d72e
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 17:35:32 +0300

    added code

--- sources/features/main-navigation-impl/build.gradle.kts
+++ sources/features/main-navigation-impl/build.gradle.kts
@@ -1,12 +1,32 @@
 plugins {
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "evgenii.goncharov.econome.main_navigation_impl"
 
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = extra["composeVersion"] as String
+    }
 }
 
 dependencies {
     api(project(":sources:features:main-navigation-api"))
 
+    implementation((project(":sources:engine:di-core")))
+    implementation(project(":sources:engine:navigation"))
+    implementation(project(":sources:core:common"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.app.compat)
+    implementation(libs.cicerone)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+    implementation(libs.fragment.ktx)
 }
