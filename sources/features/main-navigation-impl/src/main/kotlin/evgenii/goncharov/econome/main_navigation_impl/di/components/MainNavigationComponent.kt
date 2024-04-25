a09108f585d9a78e33634d68cf051848c320b131
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-25 17:50:45 +0300

    added code

---
+++ sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/di/components/MainNavigationComponent.kt
@@ -0,0 +1,14 @@
+package evgenii.goncharov.econome.main_navigation_impl.di.components
+
+import dagger.Component
+import evgenii.goncharov.econome.main_navigation_impl.di.contracts.MainNavigationInternal
+
+@Component
+internal interface MainNavigationComponent : MainNavigationInternal {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): MainNavigationComponent
+    }
+}
---
+++ sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/di/contracts/MainNavigationInternal.kt
@@ -0,0 +1,5 @@
+package evgenii.goncharov.econome.main_navigation_impl.di.contracts
+
+import evgenii.goncharov.econome.main_navigation.di.MainNavigationApi
+
+internal interface MainNavigationInternal : MainNavigationApi
