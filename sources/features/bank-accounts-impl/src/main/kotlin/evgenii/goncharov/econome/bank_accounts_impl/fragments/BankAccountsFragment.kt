a3bab2cbd2715684f10da11ee3fac679db805dae
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 21:49:31 +0300

    added code

--- sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/BankAccountsFragment.kt
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/BankAccountsFragment.kt
@@ -1,8 +1,34 @@
 package evgenii.goncharov.econome.bank_accounts_impl.fragments
 
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.bank_accounts_api.di.BankAccountsApi
+import evgenii.goncharov.econome.bank_accounts_impl.di.contracts.BankAccountsInternal
+import evgenii.goncharov.econome.bank_accounts_impl.ui.BankAccountsScreen
+import evgenii.goncharov.econome.bank_accounts_impl.view.models.BankAccountsViewModel
 import evgenii.goncharov.econome.di_core.CoreFragment
 
 internal class BankAccountsFragment : CoreFragment() {
 
+    private val dependency: BankAccountsInternal by lazy {
+        getFeatureApi(BankAccountsApi::class.java) as BankAccountsInternal
+    }
+    private val viewModel: BankAccountsViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
 
+    @Composable
+    override fun InitContent() {
+        viewModel
+        BankAccountsScreen()
+    }
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(BankAccountsApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = BankAccountsFragment()
+    }
 }
