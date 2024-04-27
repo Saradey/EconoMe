166c8d5a7ff1ade386ca70506d4e51c05cb25c6f
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 21:06:18 +0300

    added code

---
+++ sources/features/finance-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/finance_analysis_impl/view/models/FinanceAnalyticsAllSpendingViewModel.kt
@@ -0,0 +1,7 @@
+package evgenii.goncharov.econome.finance_analysis_impl.view.models
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+internal class FinanceAnalyticsAllSpendingViewModel @Inject constructor() : ViewModel() {
+}
