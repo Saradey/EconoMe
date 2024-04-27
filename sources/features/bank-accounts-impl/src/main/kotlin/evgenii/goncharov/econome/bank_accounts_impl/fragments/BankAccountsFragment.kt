a89a6906ac88a660a3c5e987aadbea7d2f3d1851
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 21:47:30 +0300

    added code

---
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/BankAccountsFragment.kt
@@ -0,0 +1,8 @@
+package evgenii.goncharov.econome.bank_accounts_impl.fragments
+
+import evgenii.goncharov.econome.di_core.CoreFragment
+
+internal class BankAccountsFragment : CoreFragment() {
+
+
+}
