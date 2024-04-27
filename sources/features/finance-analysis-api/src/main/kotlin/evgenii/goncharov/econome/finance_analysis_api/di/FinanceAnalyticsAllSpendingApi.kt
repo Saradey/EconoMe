eea62dd4bee18beb53d23a742b7115940b03c423
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 20:54:41 +0300

    added code

---
+++ sources/features/finance-analysis-api/src/main/kotlin/evgenii/goncharov/econome/finance_analysis_api/di/FinanceAnalyticsAllSpendingApi.kt
@@ -0,0 +1,5 @@
+package evgenii.goncharov.econome.finance_analysis_api.di
+
+import evgenii.goncharov.econome.di_core.contracts.ReleasableApi
+
+public interface FinanceAnalyticsAllSpendingApi : ReleasableApi
