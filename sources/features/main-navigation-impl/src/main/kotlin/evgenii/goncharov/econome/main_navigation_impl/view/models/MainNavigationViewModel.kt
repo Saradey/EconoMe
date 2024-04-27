ca4bdc6cfa7febb3bcfffb1ac85c2797e708b485
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 22:26:01 +0300

    added code

--- sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/view/models/MainNavigationViewModel.kt
+++ sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/view/models/MainNavigationViewModel.kt
@@ -3,12 +3,14 @@ package evgenii.goncharov.econome.main_navigation_impl.view.models
 import androidx.lifecycle.ViewModel
 import evgenii.goncharov.econome.all_spending_api.navigation.AllSpendingLauncher
 import evgenii.goncharov.econome.bank_accounts_api.navigation.BankAccountsLauncher
+import evgenii.goncharov.econome.finance_analysis_api.navigation.FinanceAnalyticsAllSpendingLauncher
 import evgenii.goncharov.econome.main_api.navigation.MainLauncher
 import javax.inject.Inject
 
 internal class MainNavigationViewModel @Inject constructor(
     private val mainLauncher: MainLauncher,
     private val allSpendingLauncher: AllSpendingLauncher,
+    private val financeAnalyticsAllSpendingLauncher: FinanceAnalyticsAllSpendingLauncher,
     private val bankAccountsLauncher: BankAccountsLauncher
 ) : ViewModel() {
 
