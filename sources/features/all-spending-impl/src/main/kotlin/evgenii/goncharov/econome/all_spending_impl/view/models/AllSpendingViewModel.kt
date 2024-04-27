27244a93f1b370ea2394fa9d02aec4a601ba3d31
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 20:33:30 +0300

    added code

---
+++ sources/features/all-spending-impl/src/main/kotlin/evgenii/goncharov/econome/all_spending_impl/view/models/AllSpendingViewModel.kt
@@ -0,0 +1,7 @@
+package evgenii.goncharov.econome.all_spending_impl.view.models
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+internal class AllSpendingViewModel @Inject constructor() : ViewModel() {
+}
