08c26d51ecc9dbfce36d5debb4cb9784b617d7b7
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-27 21:07:49 +0300

    added code

---
+++ sources/features/finance-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/finance_analysis_impl/di/holders/FinanceAnalyticsAllSpendingHolder.kt
@@ -0,0 +1,15 @@
+package evgenii.goncharov.econome.finance_analysis_impl.di.holders
+
+import evgenii.goncharov.econome.di_core.containers.FeatureContainer
+import evgenii.goncharov.econome.di_core.holders.FeatureHolder
+import evgenii.goncharov.econome.finance_analysis_api.di.FinanceAnalyticsAllSpendingApi
+import evgenii.goncharov.econome.finance_analysis_impl.di.components.DaggerFinanceAnalyticsAllSpendingComponent
+import javax.inject.Inject
+
+public class FinanceAnalyticsAllSpendingHolder @Inject constructor(container: FeatureContainer) :
+    FeatureHolder<FinanceAnalyticsAllSpendingApi>(container) {
+
+    override fun buildComponent(): FinanceAnalyticsAllSpendingApi {
+        return DaggerFinanceAnalyticsAllSpendingComponent.factory().create()
+    }
+}
