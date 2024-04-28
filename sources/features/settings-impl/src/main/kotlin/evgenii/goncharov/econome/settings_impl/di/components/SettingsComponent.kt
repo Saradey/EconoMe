13eac2a560911de51b1775f38371f3e709a0aecc
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-28 13:41:08 +0300

    added code

--- sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/di/components/SettingsComponent.kt
+++ sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/di/components/SettingsComponent.kt
@@ -1,4 +1,18 @@
 package evgenii.goncharov.econome.settings_impl.di.components
 
+import dagger.Component
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.settings_impl.di.modules.SettingsViewModelBindsModule
+
+@FeatureScope
+@Component(
+    modules = [SettingsViewModelBindsModule::class]
+)
 internal interface SettingsComponent {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): SettingsComponent
+    }
 }
--- sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/di/modules/SettingsViewModelBindsModule.kt
+++ sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/di/modules/SettingsViewModelBindsModule.kt
@@ -1,7 +1,22 @@
 package evgenii.goncharov.econome.settings_impl.di.modules
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import evgenii.goncharov.econome.common.MultiViewModelFactory
+import evgenii.goncharov.econome.di_core.di.keys.ViewModelKey
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.settings_impl.view.models.SettingsViewModel
 
 @Module
 internal interface SettingsViewModelBindsModule {
+
+    @[Binds FeatureScope]
+    fun bindMultiViewModelFactory(factory: MultiViewModelFactory): ViewModelProvider.Factory
+
+    @[Binds FeatureScope]
+    @[IntoMap ViewModelKey(SettingsViewModel::class)]
+    fun bindSettingsViewModel(viewModel: SettingsViewModel): ViewModel
 }
