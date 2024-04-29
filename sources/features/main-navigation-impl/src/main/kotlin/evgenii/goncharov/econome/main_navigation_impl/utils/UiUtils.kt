d4f9fa510ff9a9280d62820f293b7d066b79317a
Evgeny Goncharov <jtgn@yandex.ru>
2024-04-29 18:42:37 +0300

    added code

--- sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/utils/UiUtils.kt
+++ sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/utils/UiUtils.kt
@@ -1,28 +1,30 @@
 package evgenii.goncharov.econome.main_navigation_impl.utils
 
+import evgenii.goncharov.econome.main_navigation_impl.R
 import evgenii.goncharov.econome.main_navigation_impl.models.MainBottomNavItem
+import evgenii.goncharov.econome.ui_kit.UiKitString
 
 internal fun createBottomNavigationItems(): List<MainBottomNavItem> {
     return listOf(
         MainBottomNavItem(
-            name = "",
-            title = -1,
-            icon = -1
+            name = "main",
+            title = R.string.item_1_main,
+            icon = UiKitString.icon_stub
         ),
         MainBottomNavItem(
-            name = "",
-            title = -1,
-            icon = -1
+            name = "all_spending",
+            title = R.string.item_2_all_spending,
+            icon = UiKitString.icon_stub
         ),
         MainBottomNavItem(
-            name = "",
-            title = -1,
-            icon = -1
+            name = "all_spending_analysis",
+            title = R.string.item_3_all_spending_analysis,
+            icon = UiKitString.icon_stub
         ),
         MainBottomNavItem(
-            name = "",
-            title = -1,
-            icon = -1
+            name = "account_state",
+            title = R.string.item_4_account_state,
+            icon = UiKitString.icon_stub
         )
     )
 }
