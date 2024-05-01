c77938ac622a14ae0e25187298c0d375ef3a4fe6
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-01 12:10:59 +0300

    added code

---
+++ sources/features/main-navigation-impl/src/main/kotlin/evgenii/goncharov/econome/main_navigation_impl/navigation/BottomMenuOnBackPressed.kt
@@ -0,0 +1,16 @@
+package evgenii.goncharov.econome.main_navigation_impl.navigation
+
+import androidx.activity.OnBackPressedCallback
+import com.github.terrakok.cicerone.Router
+import evgenii.goncharov.econome.navigation.utils.DEEP_ROUTER
+import javax.inject.Inject
+import javax.inject.Named
+
+internal class BottomMenuOnBackPressed @Inject constructor(
+    @Named(DEEP_ROUTER) private val deepRouter: Router
+) : OnBackPressedCallback(true) {
+
+    override fun handleOnBackPressed() {
+        deepRouter.backTo(null)
+    }
+}
