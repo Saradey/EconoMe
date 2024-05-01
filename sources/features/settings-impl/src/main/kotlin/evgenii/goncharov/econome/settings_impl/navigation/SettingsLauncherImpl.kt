f696e2aa09f0f1b4318535e49c5e193f2e32e68c
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-01 17:41:00 +0300

    added code

--- sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/navigation/SettingsLauncherImpl.kt
+++ sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/navigation/SettingsLauncherImpl.kt
@@ -1,9 +1,16 @@
 package evgenii.goncharov.econome.settings_impl.navigation
 
+import com.github.terrakok.cicerone.Router
+import evgenii.goncharov.econome.navigation.utils.GLOBAL_ROUTER
 import evgenii.goncharov.econome.settings_api.navigation.SettingsLauncher
 import javax.inject.Inject
+import javax.inject.Named
 
-public class SettingsLauncherImpl @Inject constructor() : SettingsLauncher {
+public class SettingsLauncherImpl @Inject constructor(
+    @Named(GLOBAL_ROUTER) private val globalRouter: Router
+) : SettingsLauncher {
 
-    override fun launchSettings() {}
+    override fun launchSettings() {
+        globalRouter.navigateTo(SettingsNavigationScreens.startSettings())
+    }
 }
