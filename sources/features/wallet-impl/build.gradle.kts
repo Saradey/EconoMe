e0f3f6c7fb99f272784914d4061f3dea3cc9dce2
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-02 20:41:49 +0300

    added code

--- sources/features/wallet-impl/build.gradle.kts
+++ sources/features/wallet-impl/build.gradle.kts
@@ -1,10 +1,30 @@
 plugins {
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "evgenii.goncharov.econome.wallet_impl"
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = extra["composeVersion"] as String
+    }
 }
 
 dependencies {
     api(project(":sources:features:wallet-api"))
+
+    implementation((project(":sources:engine:di-core")))
+    implementation(project(":sources:engine:navigation"))
+    implementation(project(":sources:core:common"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.cicerone)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+    implementation(libs.fragment.ktx)
 }
