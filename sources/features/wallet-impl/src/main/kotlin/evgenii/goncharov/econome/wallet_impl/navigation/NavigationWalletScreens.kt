d5623df125626c178e49c1a6ccf93ed9416e7b7f
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-02 20:46:08 +0300

    added code

---
+++ sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/navigation/NavigationWalletScreens.kt
@@ -0,0 +1,4 @@
+package evgenii.goncharov.econome.wallet_impl.navigation
+
+internal object NavigationWalletScreens {
+}
---
+++ sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/navigation/WalletLauncherImpl.kt
@@ -0,0 +1,7 @@
+package evgenii.goncharov.econome.wallet_impl.navigation
+
+import evgenii.goncharov.econome.wallet_api.navigation.WalletLauncher
+import javax.inject.Inject
+
+public class WalletLauncherImpl @Inject constructor() : WalletLauncher {
+}
