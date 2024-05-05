3b2c3881902571cb1d451bf099fe61880e5fa9c6
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-05 16:22:16 +0300

    added code

--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/MainActivityViewModel.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/MainActivityViewModel.kt
@@ -12,8 +12,8 @@ internal class MainActivityViewModel @Inject constructor(
 
 
     fun appStart() {
-        userLauncher.launchUserCreator()
-//        userLauncher.launchUserChoosing()
+//        userLauncher.launchUserCreator()
 //        mainNavigationLauncher.launchMainNavigation()
+//        userLauncher.launchUserChoosing()
     }
 }
