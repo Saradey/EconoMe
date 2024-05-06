fc73a7a31b17e45abbd09c1cc6a904fa76ecd070
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 13:17:08 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/AddSpendingScreen.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/AddSpendingScreen.kt
@@ -1,7 +1,15 @@
 package evgenii.goncharov.econome.spending_impl.ui
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun AddSpendingScreen() {
+    Text(
+        text = "3. AddSpendingDialogFragment Диалоговое окно добавления расхода",
+        color = Color.White,
+        fontSize = 20.sp
+    )
 }
