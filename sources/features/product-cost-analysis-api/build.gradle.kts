253d78c935ef635aa6fd8567dc594e975fea4deb
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 18:49:14 +0300

    added code

--- sources/features/product-cost-analysis-api/build.gradle.kts
+++ sources/features/product-cost-analysis-api/build.gradle.kts
@@ -1,9 +1,7 @@
-plugins {
-}
+plugins {}
 
 android {
     namespace = "evgenii.goncharov.econome.product_cost_analysis_api"
-
 }
 
 dependencies {
--- sources/features/product-cost-analysis-impl/build.gradle.kts
+++ sources/features/product-cost-analysis-impl/build.gradle.kts
@@ -1,12 +1,9 @@
-plugins {
-}
+plugins {}
 
 android {
     namespace = "evgenii.goncharov.econome.product_cost_analysis_impl"
-
 }
 
 dependencies {
     api(project(":sources:features:product-cost-analysis-api"))
-
 }
