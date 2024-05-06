fa0ec0807da7e9163d43b37ec0d477bc99efc91a
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 13:49:12 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/di/holders/SpendingInfoHolder.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/di/holders/SpendingInfoHolder.kt
@@ -1,4 +1,15 @@
 package evgenii.goncharov.econome.spending_impl.di.holders
 
-public class SpendingInfoHolder {
+import evgenii.goncharov.econome.di_core.containers.FeatureContainer
+import evgenii.goncharov.econome.di_core.holders.FeatureHolder
+import evgenii.goncharov.econome.spending_api.di.SpendingInfoApi
+import evgenii.goncharov.econome.spending_impl.di.components.DaggerSpendingInfoComponent
+import javax.inject.Inject
+
+public class SpendingInfoHolder @Inject constructor(container: FeatureContainer) :
+    FeatureHolder<SpendingInfoApi>(container) {
+
+    override fun buildComponent(): SpendingInfoApi {
+        return DaggerSpendingInfoComponent.factory().create()
+    }
 }
