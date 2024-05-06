49721daea99e38a71d63131d5ed9df2d6d0af7a2
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 13:50:24 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/SpendingInfoScreen.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/SpendingInfoScreen.kt
@@ -1,7 +1,15 @@
 package evgenii.goncharov.econome.spending_impl.ui
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun SpendingInfoScreen() {
+    Text(
+        text = "5. SpendingInfoFragment Экран информации по расходу",
+        color = Color.White,
+        fontSize = 20.sp
+    )
 }
