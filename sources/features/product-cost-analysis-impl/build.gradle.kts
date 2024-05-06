f54ef3905e4d57da990f9b3c006cf6658791e125
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 19:08:08 +0300

    added code

--- sources/features/product-cost-analysis-impl/build.gradle.kts
+++ sources/features/product-cost-analysis-impl/build.gradle.kts
@@ -1,9 +1,30 @@
-plugins {}
+plugins {
+    id("kotlin-kapt")
+}
 
 android {
     namespace = "evgenii.goncharov.econome.product_cost_analysis_impl"
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = extra["composeVersion"] as String
+    }
 }
 
 dependencies {
     api(project(":sources:features:product-cost-analysis-api"))
+
+    implementation(project(":sources:engine:di-core"))
+    implementation(project(":sources:engine:navigation"))
+    implementation(project(":sources:core:common"))
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.cicerone)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+    implementation(libs.fragment.ktx)
 }
