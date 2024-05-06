3a73292b6f5a35fd81e562cce33251ddc630400d
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-06 19:18:34 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/ListShopsFragment.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/ListShopsFragment.kt
@@ -1,7 +1,34 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.fragments
 
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.di_core.CoreFragment
+import evgenii.goncharov.econome.product_cost_analysis_api.di.ListShopsApi
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.ListShopsInternal
+import evgenii.goncharov.econome.product_cost_analysis_impl.ui.ListShopsScreen
+import evgenii.goncharov.econome.product_cost_analysis_impl.view.models.ListShopsViewModel
+
 /**
  * 18.
  */
-internal class ListShopsFragment {
+internal class ListShopsFragment : CoreFragment() {
+
+    private val dependency: ListShopsInternal by lazy {
+        getFeatureApi(ListShopsApi::class.java) as ListShopsInternal
+    }
+    private val viewModel: ListShopsViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = ListShopsScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(ListShopsApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = ListShopsFragment()
+    }
 }
