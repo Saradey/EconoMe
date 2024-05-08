847b0aed3b0c8a055af426c6e1c19fa7880b9725
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 23:25:49 +0300

    added code

--- sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/di/components/AddPatternAccountComponent.kt
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/di/components/AddPatternAccountComponent.kt
@@ -1,6 +1,21 @@
 package evgenii.goncharov.econome.bank_accounts_impl.di.components
 
+import dagger.Component
 import evgenii.goncharov.econome.bank_accounts_impl.di.contracts.AddPatternAccountInternal
+import evgenii.goncharov.econome.bank_accounts_impl.di.modules.AddPatternAccountBindModule
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
 
+@FeatureScope
+@Component(
+    modules = [
+        AddPatternAccountBindModule::class
+    ]
+)
 internal interface AddPatternAccountComponent : AddPatternAccountInternal {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): AddPatternAccountComponent
+    }
 }
