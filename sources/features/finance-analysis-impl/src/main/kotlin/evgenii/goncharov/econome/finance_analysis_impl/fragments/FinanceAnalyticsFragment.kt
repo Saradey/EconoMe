30c660cd7c9b652f806a68547164b99229d7f914
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 22:11:34 +0300

    added code

--- sources/features/finance-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/finance_analysis_impl/fragments/FinanceAnalyticsFragment.kt
+++ sources/features/finance-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/finance_analysis_impl/fragments/FinanceAnalyticsFragment.kt
@@ -1,4 +1,31 @@
 package evgenii.goncharov.econome.finance_analysis_impl.fragments
 
-internal class FinanceAnalyticsFragment {
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.di_core.CoreFragment
+import evgenii.goncharov.econome.finance_analysis_api.di.FinanceAnalyticsApi
+import evgenii.goncharov.econome.finance_analysis_impl.di.contracts.FinanceAnalyticsInternal
+import evgenii.goncharov.econome.finance_analysis_impl.ui.FinanceAnalyticsScreen
+import evgenii.goncharov.econome.finance_analysis_impl.view.models.FinanceAnalyticsViewModel
+
+internal class FinanceAnalyticsFragment : CoreFragment() {
+
+    private val dependency: FinanceAnalyticsInternal by lazy {
+        getFeatureApi(FinanceAnalyticsApi::class.java) as FinanceAnalyticsInternal
+    }
+    private val viewModel: FinanceAnalyticsViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = FinanceAnalyticsScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(FinanceAnalyticsApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = FinanceAnalyticsFragment()
+    }
 }
