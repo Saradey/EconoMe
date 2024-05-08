6e0069c94876d36a531cf91fd59d3721ff3e0472
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 23:48:05 +0300

    added code

--- sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/ui/SettingsScreen.kt
+++ sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/ui/SettingsScreen.kt
@@ -2,6 +2,7 @@ package evgenii.goncharov.econome.settings_impl.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,7 +10,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 @Composable
-internal fun SettingsScreen() {
+internal fun SettingsScreen(
+    goToUserCreator: () -> Unit,
+    goToUserChoosing: () -> Unit,
+    goToWalletCreator: () -> Unit,
+    goToAllWallet: () -> Unit
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -19,5 +25,25 @@ internal fun SettingsScreen() {
             color = Color.White,
             fontSize = 20.sp
         )
+        Button(
+            onClick = goToUserCreator,
+        ) {
+            Text("Go to 0. Экран создания аккаунта")
+        }
+        Button(
+            onClick = goToUserChoosing,
+        ) {
+            Text("Go to 15. Экран выбора пользователя")
+        }
+        Button(
+            onClick = goToWalletCreator,
+        ) {
+            Text("Go to 4. Экран создания кошелька расходов")
+        }
+        Button(
+            onClick = goToAllWallet,
+        ) {
+            Text("Go to 16. Экран всех кошельков")
+        }
     }
 }
