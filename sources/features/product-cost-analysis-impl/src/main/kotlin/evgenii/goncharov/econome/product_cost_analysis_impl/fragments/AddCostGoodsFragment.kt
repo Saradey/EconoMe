8ccef609e16cf4da36640a0e3fd5db64c8331810
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 21:38:28 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/AddCostGoodsFragment.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/AddCostGoodsFragment.kt
@@ -1,4 +1,30 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.fragments
 
-internal class AddCostGoodsFragment {
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.di_core.CoreFragment
+import evgenii.goncharov.econome.product_cost_analysis_api.di.AddCostGoodsApi
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.AddCostGoodsInternal
+import evgenii.goncharov.econome.product_cost_analysis_impl.ui.AddCostGoodsScreen
+import evgenii.goncharov.econome.product_cost_analysis_impl.view.models.AddCostGoodsViewModel
+
+internal class AddCostGoodsFragment : CoreFragment() {
+
+    private val dependency: AddCostGoodsInternal by lazy {
+        getFeatureApi(AddCostGoodsApi::class.java) as AddCostGoodsInternal
+    }
+    private val viewModel: AddCostGoodsViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = AddCostGoodsScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(AddCostGoodsApi::class.java)
+    }
+
+    companion object {
+        fun newInstance() = AddCostGoodsFragment()
+    }
 }
