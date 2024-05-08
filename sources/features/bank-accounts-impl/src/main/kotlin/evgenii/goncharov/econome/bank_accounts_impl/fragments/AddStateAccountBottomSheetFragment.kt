b4ae0d273ed16d57da1d03076115515c47c04095
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 23:02:18 +0300

    added code

--- sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/AddStateAccountBottomSheetFragment.kt
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/AddStateAccountBottomSheetFragment.kt
@@ -1,4 +1,7 @@
 package evgenii.goncharov.econome.bank_accounts_impl.fragments
 
+/**
+ * 8. Screen
+ */
 internal class AddStateAccountBottomSheetFragment {
 }
--- sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/ui/AddStateAccountScreen.kt
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/ui/AddStateAccountScreen.kt
@@ -1,7 +1,23 @@
 package evgenii.goncharov.econome.bank_accounts_impl.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun AddStateAccountScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "8. AddStateAccountBottomSheetFragment Диалоговое окно по добавлению информации о денежном состоянии",
+            color = Color.White,
+            fontSize = 20.sp
+        )
+    }
 }
