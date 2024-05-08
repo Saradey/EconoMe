db262c5bbfa17f03eef9bdb8a5655783a844d1cb
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-08 23:04:06 +0300

    added code

--- sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/AddStateAccountBottomSheetFragment.kt
+++ sources/features/bank-accounts-impl/src/main/kotlin/evgenii/goncharov/econome/bank_accounts_impl/fragments/AddStateAccountBottomSheetFragment.kt
@@ -1,7 +1,34 @@
 package evgenii.goncharov.econome.bank_accounts_impl.fragments
 
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.bank_accounts_api.di.AddStateAccountApi
+import evgenii.goncharov.econome.bank_accounts_impl.di.contracts.AddStateAccountInternal
+import evgenii.goncharov.econome.bank_accounts_impl.ui.AddStateAccountScreen
+import evgenii.goncharov.econome.bank_accounts_impl.view.models.AddStateAccountViewModel
+import evgenii.goncharov.econome.di_core.CoreBottomSheetFragment
+
 /**
  * 8. Screen
  */
-internal class AddStateAccountBottomSheetFragment {
+internal class AddStateAccountBottomSheetFragment : CoreBottomSheetFragment() {
+
+    private val dependency: AddStateAccountInternal by lazy {
+        getFeatureApi(AddStateAccountApi::class.java) as AddStateAccountInternal
+    }
+    private val viewModel: AddStateAccountViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = AddStateAccountScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(AddStateAccountApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = AddStateAccountBottomSheetFragment()
+    }
 }
