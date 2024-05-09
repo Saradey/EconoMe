d9233ce7f48ae08ef2f5ae55e0cece41a53ac3bf
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 23:03:09 +0300

    added code

--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/components/AddCategoriesComponent.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/components/AddCategoriesComponent.kt
@@ -1,4 +1,21 @@
 package evgenii.goncharov.econome.common_categories_impl.di.components
 
-internal interface AddCategoriesComponent {
+import dagger.Component
+import evgenii.goncharov.econome.common_categories_impl.di.contracts.AddCategoriesInternal
+import evgenii.goncharov.econome.common_categories_impl.di.modules.AddCategoriesBindModule
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+
+@FeatureScope
+@Component(
+    modules = [
+        AddCategoriesBindModule::class
+    ]
+)
+internal interface AddCategoriesComponent : AddCategoriesInternal {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): AddCategoriesComponent
+    }
 }
