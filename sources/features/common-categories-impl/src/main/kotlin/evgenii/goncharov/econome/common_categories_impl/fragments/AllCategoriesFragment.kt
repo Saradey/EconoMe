50137b3d6c400eb60a3d9536d515835b374745d5
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:09:07 +0300

    added code

--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/fragments/AllCategoriesFragment.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/fragments/AllCategoriesFragment.kt
@@ -1,7 +1,34 @@
 package evgenii.goncharov.econome.common_categories_impl.fragments
 
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.common_categories_api.di.AllCategoriesApi
+import evgenii.goncharov.econome.common_categories_impl.di.contracts.AllCategoriesInternal
+import evgenii.goncharov.econome.common_categories_impl.ui.AllCategoriesScreen
+import evgenii.goncharov.econome.common_categories_impl.view.models.AllCategoriesViewModel
+import evgenii.goncharov.econome.di_core.CoreFragment
+
 /**
  * 11. Screen
  */
-internal class AllCategoriesFragment {
+internal class AllCategoriesFragment : CoreFragment() {
+
+    private val dependency: AllCategoriesInternal by lazy {
+        getFeatureApi(AllCategoriesApi::class.java) as AllCategoriesInternal
+    }
+    private val viewModel: AllCategoriesViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = AllCategoriesScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(AllCategoriesApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = AllCategoriesFragment()
+    }
 }
