4a57f121fe8acb6e8ba46dcf4840685d167ff474
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:04:12 +0300

    added code

--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/components/AllCategoriesComponent.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/components/AllCategoriesComponent.kt
@@ -1,4 +1,20 @@
 package evgenii.goncharov.econome.common_categories_impl.di.components
 
+import dagger.Component
+import evgenii.goncharov.econome.common_categories_impl.di.modules.AllCategoriesBindModule
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+
+@FeatureScope
+@Component(
+    modules = [
+        AllCategoriesBindModule::class
+    ]
+)
 internal interface AllCategoriesComponent {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): AllCategoriesComponent
+    }
 }
--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/modules/AllCategoriesBindModule.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/di/modules/AllCategoriesBindModule.kt
@@ -1,4 +1,18 @@
 package evgenii.goncharov.econome.common_categories_impl.di.modules
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import evgenii.goncharov.econome.common.di.MultiViewModelModule
+import evgenii.goncharov.econome.common_categories_impl.view.models.AllCategoriesViewModel
+import evgenii.goncharov.econome.di_core.di.keys.ViewModelKey
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+
+@Module(includes = [MultiViewModelModule::class])
 internal interface AllCategoriesBindModule {
+
+    @[Binds FeatureScope]
+    @[IntoMap ViewModelKey(AllCategoriesViewModel::class)]
+    fun bindAllCategoriesViewModel(viewModel: AllCategoriesViewModel): ViewModel
 }
