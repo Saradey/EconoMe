2799c5468a76ef4a53c24a2db0d3364aebc43a68
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:07:30 +0300

    added code

--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/fragments/AllCategoriesFragment.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/fragments/AllCategoriesFragment.kt
@@ -1,4 +1,7 @@
 package evgenii.goncharov.econome.common_categories_impl.fragments
 
+/**
+ * 11. Screen
+ */
 internal class AllCategoriesFragment {
 }
--- sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/ui/AllCategoriesScreen.kt
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/ui/AllCategoriesScreen.kt
@@ -1,7 +1,23 @@
 package evgenii.goncharov.econome.common_categories_impl.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun AllCategoriesScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "11. AllCategoriesFragment Экран всех категорий",
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+    }
 }
