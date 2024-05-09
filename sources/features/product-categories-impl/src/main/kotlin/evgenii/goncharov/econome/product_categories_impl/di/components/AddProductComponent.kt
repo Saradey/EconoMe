b7ca2071935604fa554f472cd5c1bca0b2eedd0c
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:37:23 +0300

    added code

--- sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/components/AddProductComponent.kt
+++ sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/components/AddProductComponent.kt
@@ -2,13 +2,14 @@ package evgenii.goncharov.econome.product_categories_impl.di.components
 
 import dagger.Component
 import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.product_categories_impl.di.contracts.AddProductInternal
 import evgenii.goncharov.econome.product_categories_impl.di.modules.AddProductBindModule
 
 @FeatureScope
 @Component(
     modules = [AddProductBindModule::class]
 )
-internal interface AddProductComponent {
+internal interface AddProductComponent : AddProductInternal {
 
     @Component.Factory
     interface Factory {
--- sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/components/AddSubProductComponent.kt
+++ sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/components/AddSubProductComponent.kt
@@ -2,13 +2,14 @@ package evgenii.goncharov.econome.product_categories_impl.di.components
 
 import dagger.Component
 import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.product_categories_impl.di.contracts.AddSubProductInternal
 import evgenii.goncharov.econome.product_categories_impl.di.modules.AddSubProductBindModule
 
 @FeatureScope
 @Component(
     modules = [AddSubProductBindModule::class]
 )
-internal interface AddSubProductComponent {
+internal interface AddSubProductComponent : AddSubProductInternal {
 
     @Component.Factory
     interface Factory {
