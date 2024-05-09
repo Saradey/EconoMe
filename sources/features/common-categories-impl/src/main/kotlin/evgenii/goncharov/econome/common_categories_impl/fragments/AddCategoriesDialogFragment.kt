ebf2702cb3dc8e26b812b68e834edbddec7bccb6
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 23:01:21 +0300

    added code

---
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/fragments/AddCategoriesDialogFragment.kt
@@ -0,0 +1,4 @@
+package evgenii.goncharov.econome.common_categories_impl.fragments
+
+internal class AddCategoriesDialogFragment {
+}
---
+++ sources/features/common-categories-impl/src/main/kotlin/evgenii/goncharov/econome/common_categories_impl/ui/AddCategoriesScreen.kt
@@ -0,0 +1,7 @@
+package evgenii.goncharov.econome.common_categories_impl.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun AddCategoriesScreen() {
+}
