d669a33e1c794d2dba8d4bcb1528f3db167655f0
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:41:49 +0300

    added code

--- sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/ui/AddProductScreen.kt
+++ sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/ui/AddProductScreen.kt
@@ -1,7 +1,23 @@
 package evgenii.goncharov.econome.product_categories_impl.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun AddProductScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "21. AddProductFragment Экран добавления категории товаров",
+            color = Color.White,
+            fontSize = 20.sp
+        )
+    }
 }
