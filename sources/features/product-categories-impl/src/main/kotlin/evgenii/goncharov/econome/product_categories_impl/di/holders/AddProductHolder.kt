f14cab2efa927b8d1d56423931764c6c5dcd2bf0
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:39:08 +0300

    added code

--- sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/holders/AddProductHolder.kt
+++ sources/features/product-categories-impl/src/main/kotlin/evgenii/goncharov/econome/product_categories_impl/di/holders/AddProductHolder.kt
@@ -1,4 +1,15 @@
 package evgenii.goncharov.econome.product_categories_impl.di.holders
 
-public class AddProductHolder {
+import evgenii.goncharov.econome.di_core.containers.FeatureContainer
+import evgenii.goncharov.econome.di_core.holders.FeatureHolder
+import evgenii.goncharov.econome.product_categories_api.di.AddProductApi
+import evgenii.goncharov.econome.product_categories_impl.di.components.DaggerAddProductComponent
+import javax.inject.Inject
+
+public class AddProductHolder @Inject constructor(container: FeatureContainer) :
+    FeatureHolder<AddProductApi>(container) {
+
+    override fun buildComponent(): AddProductApi {
+        return DaggerAddProductComponent.factory().create()
+    }
 }
