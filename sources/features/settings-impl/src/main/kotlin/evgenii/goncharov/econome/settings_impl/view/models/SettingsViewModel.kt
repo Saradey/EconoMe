945e67fe55d51fdaca00c069ec6ffabb792f712e
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 22:50:17 +0300

    added code

--- sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/view/models/SettingsViewModel.kt
+++ sources/features/settings-impl/src/main/kotlin/evgenii/goncharov/econome/settings_impl/view/models/SettingsViewModel.kt
@@ -2,6 +2,7 @@ package evgenii.goncharov.econome.settings_impl.view.models
 
 import androidx.lifecycle.ViewModel
 import evgenii.goncharov.econome.common_categories_api.navigation.CommonCategoriesLauncher
+import evgenii.goncharov.econome.product_categories_api.navigation.ProductCategoriesLauncher
 import evgenii.goncharov.econome.user_api.navigation.UserLauncher
 import evgenii.goncharov.econome.wallet_api.navigation.WalletLauncher
 import javax.inject.Inject
@@ -9,7 +10,8 @@ import javax.inject.Inject
 internal class SettingsViewModel @Inject constructor(
     private val userLauncher: UserLauncher,
     private val walletLauncher: WalletLauncher,
-    private val commonCategoriesLauncher: CommonCategoriesLauncher
+    private val commonCategoriesLauncher: CommonCategoriesLauncher,
+    private val productCategoriesLauncher: ProductCategoriesLauncher
 ) : ViewModel() {
 
     fun navigateUserCreator() {
@@ -30,5 +32,13 @@ internal class SettingsViewModel @Inject constructor(
 
     fun navigateAllCategories() {
         commonCategoriesLauncher.launchAllCategories()
+    }
+
+    fun navigateAddProduct() {
+        productCategoriesLauncher.launchAddProduct()
+    }
+
+    fun navigateAddSubProduct() {
+        productCategoriesLauncher.launchAddSubProduct()
     }
 }
