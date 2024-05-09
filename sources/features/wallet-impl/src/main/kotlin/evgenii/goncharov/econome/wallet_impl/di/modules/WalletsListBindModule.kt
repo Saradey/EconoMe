374a476f4056f8f04c9b5e665024fcce4b8f75e3
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 21:41:02 +0300

    added code

--- sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/di/modules/WalletsListBindModule.kt
+++ sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/di/modules/WalletsListBindModule.kt
@@ -1,4 +1,18 @@
 package evgenii.goncharov.econome.wallet_impl.di.modules
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import evgenii.goncharov.econome.common.di.MultiViewModelModule
+import evgenii.goncharov.econome.di_core.di.keys.ViewModelKey
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.wallet_impl.view.models.WalletsListViewModel
+
+@Module(includes = [MultiViewModelModule::class])
 internal interface WalletsListBindModule {
+
+    @[Binds FeatureScope]
+    @[IntoMap ViewModelKey(WalletsListViewModel::class)]
+    fun bindWalletsListViewModel(viewModel: WalletsListViewModel): ViewModel
 }
