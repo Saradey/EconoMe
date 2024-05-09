58399c102522241a745bf571b21ba35f352516e7
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-09 21:49:11 +0300

    added code

---
+++ sources/features/common-categories-api/src/main/kotlin/evgenii/goncharov/econome/common_categories_api/di/AllCategoriesApi.kt
@@ -0,0 +1,4 @@
+package evgenii.goncharov.econome.common_categories_api.di
+
+public interface AllCategoriesApi {
+}
---
+++ sources/features/common-categories-api/src/main/kotlin/evgenii/goncharov/econome/common_categories_api/navigation/CommonCategoriesLauncher.kt
@@ -0,0 +1,4 @@
+package evgenii.goncharov.econome.common_categories_api.navigation
+
+public interface CommonCategoriesLauncher {
+}
