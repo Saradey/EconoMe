1a1fd0ae9fdf6eaef3615583b606034d01ba24f6
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 12:46:06 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/AppShopComponent.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/AppShopComponent.kt
@@ -1,4 +1,19 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.di.components
 
-internal interface AppShopComponent {
+import dagger.Component
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.AppShopInternal
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.modules.AppShopBindModule
+
+@FeatureScope
+@Component(
+    modules = [AppShopBindModule::class]
+)
+internal interface AppShopComponent : AppShopInternal {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): AppShopComponent
+    }
 }
