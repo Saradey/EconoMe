d44e0d04d55ae85caf2c2e1d936279e6cc19fab0
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 22:50:45 +0300

    added code

--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/di/modules/MainActivityBindsModule.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/di/modules/MainActivityBindsModule.kt
@@ -10,6 +10,8 @@ import evgenii.goncharov.econome.main_activity.view.model.MainActivityViewModel
 import evgenii.goncharov.econome.di_core.di.keys.ViewModelKey
 import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
 import evgenii.goncharov.econome.main_activity.navigation.MainOnBackPressed
+import evgenii.goncharov.econome.main_activity.repositories.MainActivityRepository
+import evgenii.goncharov.econome.main_activity.repositories.impl.MainActivityRepositoryImpl
 
 @Module(includes = [MultiViewModelModule::class])
 internal interface MainActivityBindsModule {
@@ -20,4 +22,8 @@ internal interface MainActivityBindsModule {
 
     @[Binds FeatureScope]
     fun bindMainOnBackPressed(back: MainOnBackPressed): OnBackPressedCallback
+
+    @Binds
+    @FeatureScope
+    fun bindMainActivityRepository(repository: MainActivityRepositoryImpl): MainActivityRepository
 }
--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/repositories/impl/MainActivityRepositoryImpl.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/repositories/impl/MainActivityRepositoryImpl.kt
@@ -1,6 +1,11 @@
 package evgenii.goncharov.econome.main_activity.repositories.impl
 
+import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
 import evgenii.goncharov.econome.main_activity.repositories.MainActivityRepository
+import javax.inject.Inject
 
-internal class MainActivityRepositoryImpl constructor() : MainActivityRepository {
+internal class MainActivityRepositoryImpl @Inject constructor(
+    private val userDataStore: UserDataStore
+) : MainActivityRepository {
+    
 }
