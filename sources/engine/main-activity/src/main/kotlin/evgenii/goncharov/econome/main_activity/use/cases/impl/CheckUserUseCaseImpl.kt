c56d0e698c463fe2385ac42cfe7660681654adf7
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 23:06:42 +0300

    added code

--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/use/cases/impl/CheckUserUseCaseImpl.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/use/cases/impl/CheckUserUseCaseImpl.kt
@@ -1,9 +1,12 @@
 package evgenii.goncharov.econome.main_activity.use.cases.impl
 
+import evgenii.goncharov.econome.main_activity.repositories.MainActivityRepository
 import evgenii.goncharov.econome.main_activity.use.cases.CheckUserUseCase
 import javax.inject.Inject
 
-internal class CheckUserUseCaseImpl @Inject constructor() : CheckUserUseCase {
+internal class CheckUserUseCaseImpl @Inject constructor(
+    private val mainActivityRepository: MainActivityRepository
+) : CheckUserUseCase {
 
     override fun checkUser() {
 
--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/view/model/MainActivityViewModel.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/view/model/MainActivityViewModel.kt
@@ -1,13 +1,15 @@
 package evgenii.goncharov.econome.main_activity.view.model
 
 import androidx.lifecycle.ViewModel
+import evgenii.goncharov.econome.main_activity.use.cases.CheckUserUseCase
 import evgenii.goncharov.econome.main_navigation.navigation.MainNavigationLauncher
 import evgenii.goncharov.econome.user_api.navigation.UserLauncher
 import javax.inject.Inject
 
 internal class MainActivityViewModel @Inject constructor(
     private val userLauncher: UserLauncher,
-    private val mainNavigationLauncher: MainNavigationLauncher
+    private val mainNavigationLauncher: MainNavigationLauncher,
+    private val checkUserUseCase: CheckUserUseCase
 ) : ViewModel() {
 
 
