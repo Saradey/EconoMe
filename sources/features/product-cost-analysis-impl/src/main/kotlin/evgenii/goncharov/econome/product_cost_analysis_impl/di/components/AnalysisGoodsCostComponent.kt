29d09bfafdc1e5c38ab6e996a6aebca43e739da9
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 13:02:57 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/AnalysisGoodsCostComponent.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/AnalysisGoodsCostComponent.kt
@@ -1,6 +1,19 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.di.components
 
+import dagger.Component
+import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
 import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.AnalysisGoodsCostInternal
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.modules.AnalysisGoodsCostBindModule
 
+@FeatureScope
+@Component(
+    modules = [AnalysisGoodsCostBindModule::class]
+)
 internal interface AnalysisGoodsCostComponent : AnalysisGoodsCostInternal {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(): AnalysisGoodsCostComponent
+    }
 }
