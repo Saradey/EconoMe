cb0a5ec06266023c0163bfe365c642d064823ff4
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:05:07 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseProvideModule.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseProvideModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import evgenii.goncharov.econome.core_database_impl.common.DATA_BASE_NAME
+import evgenii.goncharov.econome.core_database_impl.dao.UserDao
 import evgenii.goncharov.econome.core_database_impl.data.base.AppDatabase
 import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
 
@@ -17,5 +18,11 @@ internal object CoreDatabaseProvideModule {
         return Room.databaseBuilder(appContext, AppDatabase::class.java, DATA_BASE_NAME)
             .fallbackToDestructiveMigration()
             .build()
+    }
+
+    @CoreScope
+    @Provides
+    fun provideUserDao(appDatabase: AppDatabase): UserDao {
+        return appDatabase.userDao()
     }
 }
