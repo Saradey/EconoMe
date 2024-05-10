fb9aa07792d68f01f740a836f400621c8b55bfcc
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 12:54:56 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/AddShopFragment.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/AddShopFragment.kt
@@ -1,4 +1,31 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.fragments
 
-internal class AddShopFragment {
+import androidx.compose.runtime.Composable
+import androidx.fragment.app.viewModels
+import evgenii.goncharov.econome.di_core.CoreFragment
+import evgenii.goncharov.econome.product_cost_analysis_api.di.AddShopApi
+import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.AddShopInternal
+import evgenii.goncharov.econome.product_cost_analysis_impl.ui.AddShopScreen
+import evgenii.goncharov.econome.product_cost_analysis_impl.view.models.AddShopViewModel
+
+internal class AddShopFragment : CoreFragment() {
+
+    private val dependency: AddShopInternal by lazy {
+        getFeatureApi(AddShopApi::class.java) as AddShopInternal
+    }
+    private val viewModel: AddShopViewModel by viewModels {
+        dependency.provideViewModelFactory()
+    }
+
+    @Composable
+    override fun InitContent() = AddShopScreen()
+
+    override fun releaseDependencies() {
+        releaseFeatureApi(AddShopApi::class.java)
+    }
+
+    companion object {
+
+        fun newInstance() = AddShopFragment()
+    }
 }
