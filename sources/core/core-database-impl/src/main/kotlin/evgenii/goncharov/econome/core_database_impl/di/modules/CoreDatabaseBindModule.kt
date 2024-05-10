a539fd5b4a079715997ac668def4dffa0b0fbdff
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 22:09:27 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseBindModule.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseBindModule.kt
@@ -1,9 +1,15 @@
 package evgenii.goncharov.econome.core_database_impl.di.modules
 
+import dagger.Binds
 import dagger.Module
+import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
+import evgenii.goncharov.econome.core_database_impl.stores.UserDataStoreImpl
+import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
 
 @Module
 internal interface CoreDatabaseBindModule {
 
-
+    @CoreScope
+    @Binds
+    fun bindUserDataStore(store: UserDataStoreImpl): UserDataStore
 }
--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/UserDataStoreImpl.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/UserDataStoreImpl.kt
@@ -2,9 +2,12 @@ package evgenii.goncharov.econome.core_database_impl.stores
 
 import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
 import evgenii.goncharov.econome.core_database_api.dto.UserDto
+import evgenii.goncharov.econome.core_database_impl.dao.UserDao
 import javax.inject.Inject
 
-internal class UserDataStoreImpl @Inject constructor() : UserDataStore {
+internal class UserDataStoreImpl @Inject constructor(
+    private val userDao: UserDao
+) : UserDataStore {
 
     override fun getAllUsers(): List<UserDto> {
         TODO("Not yet implemented")
