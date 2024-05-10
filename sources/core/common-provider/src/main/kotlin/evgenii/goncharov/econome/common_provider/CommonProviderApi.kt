2d372ebb1d078ab11793f6b5d13a8f57740ddb49
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:14:27 +0300

    added code

--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderApi.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderApi.kt
@@ -1,6 +1,8 @@
 package evgenii.goncharov.econome.common_provider
 
+import android.content.Context
+
 public interface CommonProviderApi {
 
-
+    public fun provideAppContext(): Context
 }
--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderComponent.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderComponent.kt
@@ -1,9 +1,17 @@
 package evgenii.goncharov.econome.common_provider
 
+import android.content.Context
+import dagger.BindsInstance
 import dagger.Component
 import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
 
 @CoreScope
 @Component
-internal interface CommonProviderComponent : CommonProviderApi{
+internal interface CommonProviderComponent : CommonProviderApi {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(@BindsInstance appContext: Context): CommonProviderComponent
+    }
 }
