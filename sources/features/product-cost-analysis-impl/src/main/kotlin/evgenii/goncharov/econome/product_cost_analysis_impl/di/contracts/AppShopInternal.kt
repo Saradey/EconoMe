4640c929e57d4d4a443b4851ebb40c832b6639af
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 12:43:39 +0300

    added code

---
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/contracts/AppShopInternal.kt
@@ -0,0 +1,5 @@
+package evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts
+
+import evgenii.goncharov.econome.product_cost_analysis_api.di.AppShopApi
+
+internal interface AppShopInternal : AppShopApi
---
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/fragments/AppShopFragment.kt
@@ -0,0 +1,4 @@
+package evgenii.goncharov.econome.product_cost_analysis_impl.fragments
+
+internal class AppShopFragment {
+}
