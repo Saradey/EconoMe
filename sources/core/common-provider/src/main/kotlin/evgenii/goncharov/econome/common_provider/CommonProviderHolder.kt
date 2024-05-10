42e88002cfcee4da0e167dd4a2d98abe18416462
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:16:27 +0300

    added code

--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderHolder.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderHolder.kt
@@ -1,5 +1,18 @@
 package evgenii.goncharov.econome.common_provider
 
-public class CommonProviderHolder {
+import android.content.Context
+import evgenii.goncharov.econome.di_core.containers.FeatureContainerManager
+import evgenii.goncharov.econome.di_core.holders.BaseHolder
+import javax.inject.Inject
 
+public class CommonProviderHolder @Inject constructor(
+    featureContainer: FeatureContainerManager,
+    private val appContext: Context
+) : BaseHolder<CommonProviderApi>(featureContainer) {
+
+    override fun buildComponent(): CommonProviderApi {
+        return DaggerCommonProviderComponent.factory().create(
+            appContext = appContext
+        )
+    }
 }
