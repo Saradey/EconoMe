0f045dfb0d988a736f0441f20d5e915c252f666a
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:23:05 +0300

    added code

---
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/holder/CoreDatabaseHolder.kt
@@ -0,0 +1,19 @@
+package evgenii.goncharov.econome.core_database_impl.di.holder
+
+import evgenii.goncharov.econome.common_provider.CommonProviderApi
+import evgenii.goncharov.econome.core_database_api.di.CoreDatabaseApi
+import evgenii.goncharov.econome.core_database_impl.di.DaggerCoreDatabaseComponent
+import evgenii.goncharov.econome.di_core.containers.FeatureContainerManager
+import evgenii.goncharov.econome.di_core.holders.BaseHolder
+import javax.inject.Inject
+
+public class CoreDatabaseHolder @Inject constructor(
+    featureContainer: FeatureContainerManager
+) : BaseHolder<CoreDatabaseApi>(featureContainer) {
+
+    override fun buildComponent(): CoreDatabaseApi {
+        return DaggerCoreDatabaseComponent.builder()
+            .commonProviderApi(getGlobalComponent(CommonProviderApi::class.java))
+            .build()
+    }
+}
