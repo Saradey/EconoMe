0635dc29f44b6e45fd6c7010184a1467fbd808b7
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 23:10:59 +0300

    added code

--- sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/use/cases/CheckUserUseCase.kt
+++ sources/engine/main-activity/src/main/kotlin/evgenii/goncharov/econome/main_activity/use/cases/CheckUserUseCase.kt
@@ -1,6 +1,8 @@
 package evgenii.goncharov.econome.main_activity.use.cases
 
+import evgenii.goncharov.econome.main_activity.models.CheckUserModel
+
 internal fun interface CheckUserUseCase {
 
-    fun checkUser()
+    suspend fun checkUser(): CheckUserModel
 }
