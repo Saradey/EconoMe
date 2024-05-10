92e3e4b7335b70f6586efa42100dd1e1108f077b
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 12:40:11 +0300

    added code

--- sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/ListShopsComponent.kt
+++ sources/features/product-cost-analysis-impl/src/main/kotlin/evgenii/goncharov/econome/product_cost_analysis_impl/di/components/ListShopsComponent.kt
@@ -1,7 +1,9 @@
 package evgenii.goncharov.econome.product_cost_analysis_impl.di.components
 
+import dagger.BindsInstance
 import dagger.Component
 import evgenii.goncharov.econome.di_core.di.scopes.FeatureScope
+import evgenii.goncharov.econome.product_cost_analysis_api.navigation.ProductCostAnalysisLauncher
 import evgenii.goncharov.econome.product_cost_analysis_impl.di.contracts.ListShopsInternal
 import evgenii.goncharov.econome.product_cost_analysis_impl.di.modules.ListShopsBindsModule
 
@@ -16,6 +18,8 @@ internal interface ListShopsComponent : ListShopsInternal {
     @Component.Factory
     interface Factory {
 
-        fun create(): ListShopsComponent
+        fun create(
+            @BindsInstance productCostAnalysisLauncher: ProductCostAnalysisLauncher
+        ): ListShopsComponent
     }
 }
