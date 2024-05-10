f0231f73e7d93851e3933dcc485bf0a2287b2ddf
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:13:00 +0300

    added code

--- sources/core/common-provider/build.gradle.kts
+++ sources/core/common-provider/build.gradle.kts
@@ -1,7 +1,14 @@
-plugins {}
+plugins {
+    id("kotlin-kapt")
+}
 
 android {
     namespace = "evgenii.goncharov.econome.common_provider"
 }
 
-dependencies {}
+dependencies {
+    implementation(project(":sources:engine:di-core"))
+
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+}
--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderComponent.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/CommonProviderComponent.kt
@@ -1,5 +1,8 @@
 package evgenii.goncharov.econome.common_provider
 
+import dagger.Component
+import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
+
 @CoreScope
 @Component
 internal interface CommonProviderComponent : CommonProviderApi{
