4c8477bda9d5e9cb39314023255763459a8c3dd1
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-10 21:20:43 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/CoreDatabaseComponent.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/CoreDatabaseComponent.kt
@@ -1,12 +1,14 @@
 package evgenii.goncharov.econome.core_database_impl.di
 
 import dagger.Component
+import evgenii.goncharov.econome.common_provider.CommonProviderApi
 import evgenii.goncharov.econome.core_database_api.di.CoreDatabaseApi
 import evgenii.goncharov.econome.core_database_impl.di.modules.CoreDatabaseProvideModule
 import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
 
 @CoreScope
 @Component(
-    modules = [CoreDatabaseProvideModule::class]
+    modules = [CoreDatabaseProvideModule::class],
+    dependencies = [CommonProviderApi::class]
 )
 internal interface CoreDatabaseComponent : CoreDatabaseApi
