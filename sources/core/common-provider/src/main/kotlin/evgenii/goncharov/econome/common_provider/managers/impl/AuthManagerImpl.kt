177616ab6297f967afce698168fce40ce141ec94
Evgeny Goncharov <jtgn@yandex.ru>
2024-05-12 14:41:59 +0300

    added code

--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/managers/impl/AuthManagerImpl.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/managers/impl/AuthManagerImpl.kt
@@ -1,7 +1,13 @@
 package evgenii.goncharov.econome.common_provider.managers.impl
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import evgenii.goncharov.econome.common_provider.managers.AuthManager
 import javax.inject.Inject
 
 internal class AuthManagerImpl @Inject constructor() : AuthManager {
+
+    private val auth : FirebaseAuth = Firebase.auth
+
 }
