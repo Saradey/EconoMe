ce606bfd665271fd49f3e08eaaa875d5b0e8a972
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-05-26 13:41:43 +0300

    added code

--- sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/managers/impl/AuthManagerImpl.kt
+++ sources/core/common-provider/src/main/kotlin/evgenii/goncharov/econome/common_provider/managers/impl/AuthManagerImpl.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import evgenii.goncharov.econome.common_provider.R
 import evgenii.goncharov.econome.common_provider.managers.AuthManager
+import evgenii.goncharov.econome.common_provider.managers.ResourceManager
 import javax.inject.Inject
 
 internal class AuthManagerImpl @Inject constructor(
     context: Context,
+    private val resourceManager: ResourceManager
 ) : AuthManager {
 
     private val oneTapSignInClient: SignInClient = Identity.getSignInClient(context)
@@ -24,7 +27,15 @@ internal class AuthManagerImpl @Inject constructor(
     }
 
     private fun createAuthRequest(): BeginSignInRequest {
-        val request = BeginSignInRequest.builder().build()
+        val request = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(resourceManager.getString(R.string.server_client_id)) // Замените на ваш client ID
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .build()
         return request
     }
 }
