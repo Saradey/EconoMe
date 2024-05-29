755cd93ac1453b9cdcc0edcadb90a9e3f8a94d60
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-05-29 20:01:18 +0300

    added code

--- sources/features/user-impl/src/main/kotlin/evgenii/goncharov/econome/user_impl/view/models/UserCreatorViewModel.kt
+++ sources/features/user-impl/src/main/kotlin/evgenii/goncharov/econome/user_impl/view/models/UserCreatorViewModel.kt
@@ -6,23 +6,30 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import evgenii.goncharov.econome.common.ui.SystemEvent
 import evgenii.goncharov.econome.common_provider.managers.AuthManager
 import evgenii.goncharov.econome.common_provider.managers.ResourceManager
+import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
+import evgenii.goncharov.econome.core_database_api.dto.UserDto
 import evgenii.goncharov.econome.user_impl.R
 import evgenii.goncharov.econome.user_impl.models.UserCreatorUiState
 import evgenii.goncharov.econome.user_impl.models.UserStatusModel
 import evgenii.goncharov.econome.user_impl.use.cases.UserValidateNameUseCase
 import evgenii.goncharov.econome.wallet_api.navigation.WalletLauncher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class UserCreatorViewModel @Inject constructor(
     private val walletLauncher: WalletLauncher,
     private val userValidateNameUseCase: UserValidateNameUseCase,
     private val resourceManager: ResourceManager,
-    private val authManager: AuthManager
+    private val authManager: AuthManager,
+    private val userDataStore: UserDataStore
 ) : ViewModel() {
 
     private val _uiState: MutableState<UserCreatorUiState> = mutableStateOf(
@@ -92,6 +99,20 @@ internal class UserCreatorViewModel @Inject constructor(
 
     fun userCreated() {
         val userInputName = _uiState.value.userNameInputText
+        viewModelScope.launch {
+            saveUser("", userInputName)
+        }
+    }
 
+    private suspend fun saveUser(
+        userId: String,
+        userInputName: String
+    ) = withContext(Dispatchers.IO) {
+        userDataStore.saveNewUser(
+            UserDto(
+                uuid = userId,
+                userName = userInputName
+            )
+        )
     }
 }
