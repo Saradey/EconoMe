6e702dad1dccf10250eb122eb8f50a48d88caaab
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-05-29 21:29:29 +0300

    added code

--- sources/core/core-database-api/src/main/kotlin/evgenii/goncharov/econome/core_database_api/di/CoreDatabaseApi.kt
+++ sources/core/core-database-api/src/main/kotlin/evgenii/goncharov/econome/core_database_api/di/CoreDatabaseApi.kt
@@ -1,8 +1,11 @@
 package evgenii.goncharov.econome.core_database_api.di
 
 import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
+import evgenii.goncharov.econome.core_database_api.data.stores.WalletDataStore
 
 public interface CoreDatabaseApi {
 
     public fun provideUserDataStore() : UserDataStore
+
+    public fun provideWalletDataStore(): WalletDataStore
 }
