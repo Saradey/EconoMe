f83330527954b656d4aa42ab057550a0b63f1eb7
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-05-29 21:33:09 +0300

    added code

--- sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/fragments/WalletCreatorFragment.kt
+++ sources/features/wallet-impl/src/main/kotlin/evgenii/goncharov/econome/wallet_impl/fragments/WalletCreatorFragment.kt
@@ -1,6 +1,8 @@
 package evgenii.goncharov.econome.wallet_impl.fragments
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import evgenii.goncharov.econome.di_core.CoreFragment
 import evgenii.goncharov.econome.wallet_api.di.WalletCreatorApi
@@ -29,6 +31,14 @@ internal class WalletCreatorFragment : CoreFragment() {
 
     companion object {
 
-        fun newInstance(userUuid: String) = WalletCreatorFragment()
+        private const val USER_UUID_ARG = "USER_UUID_ARG"
+
+        fun newInstance(userUuid: String): Fragment {
+            val args = Bundle()
+            args.putString(USER_UUID_ARG, userUuid)
+            return WalletCreatorFragment().apply {
+                arguments = args
+            }
+        }
     }
 }
