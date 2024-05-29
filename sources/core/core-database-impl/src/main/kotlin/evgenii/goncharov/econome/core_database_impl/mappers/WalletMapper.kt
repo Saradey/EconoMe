1cac02492db97cef7702ffbdca36017063c5096b
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-05-29 21:26:54 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/mappers/WalletMapper.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/mappers/WalletMapper.kt
@@ -1,4 +1,16 @@
 package evgenii.goncharov.econome.core_database_impl.mappers
 
-internal class WalletMapper {
+import evgenii.goncharov.econome.core_database_api.dto.WalletDto
+import evgenii.goncharov.econome.core_database_impl.entities.WalletEntity
+import javax.inject.Inject
+
+internal class WalletMapper @Inject constructor() {
+
+    fun walletDtoToWalletEntity(walletDto: WalletDto) : WalletEntity {
+        return WalletEntity(
+            id = walletDto.id,
+            walletName = walletDto.walletName,
+            walletCodeIso2 = walletDto.walletCodeIso2
+        )
+    }
 }
--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/WalletDataStoreImpl.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/WalletDataStoreImpl.kt
@@ -3,12 +3,15 @@ package evgenii.goncharov.econome.core_database_impl.stores
 import evgenii.goncharov.econome.core_database_api.data.stores.WalletDataStore
 import evgenii.goncharov.econome.core_database_api.dto.WalletDto
 import evgenii.goncharov.econome.core_database_impl.dao.WalletDao
+import evgenii.goncharov.econome.core_database_impl.mappers.WalletMapper
 import javax.inject.Inject
 
 internal class WalletDataStoreImpl @Inject constructor(
-    private val walletDao: WalletDao
+    private val walletDao: WalletDao,
+    private val walletMapper: WalletMapper
 ) : WalletDataStore {
 
     override suspend fun saveNewWallet(dto: WalletDto) {
+        walletDao.saveWallet(walletMapper.walletDtoToWalletEntity(dto))
     }
 }
