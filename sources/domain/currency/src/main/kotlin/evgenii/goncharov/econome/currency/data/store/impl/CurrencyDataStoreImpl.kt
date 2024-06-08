5e821e40497d9e040c53ed7cf4738b5d6a0e89de
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-08 12:37:31 +0300

    added code

--- sources/domain/currency/src/main/kotlin/evgenii/goncharov/econome/currency/data/store/impl/CurrencyDataStoreImpl.kt
+++ sources/domain/currency/src/main/kotlin/evgenii/goncharov/econome/currency/data/store/impl/CurrencyDataStoreImpl.kt
@@ -1,9 +1,13 @@
 package evgenii.goncharov.econome.currency.data.store.impl
 
 import evgenii.goncharov.econome.common_provider.managers.ResourceManager
+import evgenii.goncharov.econome.currency.R
 import evgenii.goncharov.econome.currency.data.store.CurrencyDataStore
 import evgenii.goncharov.econome.currency.models.CurrencyDto
 import kotlinx.serialization.json.Json
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 import javax.inject.Inject
 
 internal class CurrencyDataStoreImpl @Inject constructor(
@@ -14,6 +18,13 @@ internal class CurrencyDataStoreImpl @Inject constructor(
 
 
     override fun getCurrencies(): List<CurrencyDto> {
+        val rawCurrency = resourceManager.getRawFile(R.raw.currency_list)
+        val rawJsonCurrency = readRawCurrency(rawCurrency)
         return emptyList()
+    }
+
+    private fun readRawCurrency(stream: InputStream) : String {
+        val reader = BufferedReader(InputStreamReader(stream))
+        return reader.use { it.readText() }
     }
 }
