60d5e7b12c3ac5b4ca8418ac2031b2419c42e5ea
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-13 22:26:25 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/CurrentWalletDataStoreImpl.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/stores/CurrentWalletDataStoreImpl.kt
@@ -1,9 +1,11 @@
 package evgenii.goncharov.econome.core_database_impl.stores
 
 import evgenii.goncharov.econome.core_database_api.data.stores.CurrentWalletDataStore
+import evgenii.goncharov.econome.core_database_impl.dao.CurrentWalletDao
 import javax.inject.Inject
 
 internal class CurrentWalletDataStoreImpl @Inject constructor(
-
+    private val currentWalletDao: CurrentWalletDao
 ) : CurrentWalletDataStore {
+
 }
