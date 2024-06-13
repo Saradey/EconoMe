abf8232ed6ebeaf073e57c8957f59bdc4b552328
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-13 21:39:49 +0300

    added code

--- sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseBindModule.kt
+++ sources/core/core-database-impl/src/main/kotlin/evgenii/goncharov/econome/core_database_impl/di/modules/CoreDatabaseBindModule.kt
@@ -2,8 +2,10 @@ package evgenii.goncharov.econome.core_database_impl.di.modules
 
 import dagger.Binds
 import dagger.Module
+import evgenii.goncharov.econome.core_database_api.data.stores.CurrentWalletDataStore
 import evgenii.goncharov.econome.core_database_api.data.stores.UserDataStore
 import evgenii.goncharov.econome.core_database_api.data.stores.WalletDataStore
+import evgenii.goncharov.econome.core_database_impl.stores.CurrentWalletDataStoreImpl
 import evgenii.goncharov.econome.core_database_impl.stores.UserDataStoreImpl
 import evgenii.goncharov.econome.core_database_impl.stores.WalletDataStoreImpl
 import evgenii.goncharov.econome.di_core.di.scopes.CoreScope
@@ -18,4 +20,8 @@ internal interface CoreDatabaseBindModule {
     @CoreScope
     @Binds
     fun bindWalletDataStore(store: WalletDataStoreImpl): WalletDataStore
+
+    @CoreScope
+    @Binds
+    fun bindCurrentWalletDataStoreImpl(store: CurrentWalletDataStoreImpl): CurrentWalletDataStore
 }
