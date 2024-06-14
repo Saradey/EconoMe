7ada747a03f5703adc96a38dd1f2b5e1d99898a0
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-14 17:04:59 +0300

    added code

--- sources/features/user-impl/build.gradle.kts
+++ sources/features/user-impl/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":sources:core:common"))
     implementation(project(":sources:core:common-provider"))
     implementation(project(":sources:core:core-database-api"))
+    implementation(project(":sources:core:current-user"))
 
     implementation(project(":sources:features:wallet-api"))
     implementation(project(":sources:features:main-navigation-api"))
