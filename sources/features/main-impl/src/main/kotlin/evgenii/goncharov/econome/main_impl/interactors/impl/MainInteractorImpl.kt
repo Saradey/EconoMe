460b778150f99b11258fc3884d7e666b0bfa75bd
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-16 13:52:11 +0300

    added code

--- sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/interactors/impl/MainInteractorImpl.kt
+++ sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/interactors/impl/MainInteractorImpl.kt
@@ -11,6 +11,17 @@ internal class MainInteractorImpl @Inject constructor(
 ) : MainInteractor {
 
     override fun checkParameters() {
+        currentUserRepository.getCurrentUserId() ?: throw IllegalArgumentException(
+            USER_ERROR_MESSAGE
+        )
+        currentWalletRepository.getCurrentWalletId() ?: throw IllegalArgumentException(
+            WALLET_ERROR_MESSAGE
+        )
+    }
 
+    private companion object {
+
+        const val USER_ERROR_MESSAGE = "Current user cannot be null"
+        const val WALLET_ERROR_MESSAGE = "Current wallet cannot be null"
     }
 }
