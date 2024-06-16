bfe127c4578dca0c0c70f04d9dd2a4aa536d33dc
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-16 15:00:51 +0300

    added code

--- sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/models/MainUiState.kt
+++ sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/models/MainUiState.kt
@@ -1,5 +1,10 @@
 package evgenii.goncharov.econome.main_impl.models
 
-internal data class MainUiState(
-    val currentUser: CurrentUserModel
-)
+internal sealed class MainUiState {
+
+    data object InitialState : MainUiState()
+
+    data class Content(
+        val currentUser: CurrentUserModel
+    ) : MainUiState()
+}
--- sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/view/models/MainViewModel.kt
+++ sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/view/models/MainViewModel.kt
@@ -3,8 +3,12 @@ package evgenii.goncharov.econome.main_impl.view.models
 import androidx.lifecycle.ViewModel
 import evgenii.goncharov.econome.main_api.navigation.MainLauncher
 import evgenii.goncharov.econome.main_impl.interactors.MainInteractor
+import evgenii.goncharov.econome.main_impl.models.MainUiState
 import evgenii.goncharov.econome.product_cost_analysis_api.navigation.ProductCostAnalysisLauncher
 import evgenii.goncharov.econome.spending_api.navigation.SpendingLauncher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 internal class MainViewModel @Inject constructor(
@@ -13,6 +17,9 @@ internal class MainViewModel @Inject constructor(
     private val productCostAnalysisLauncher: ProductCostAnalysisLauncher,
     private val mainInteractor: MainInteractor
 ) : ViewModel() {
+
+    private val _uiState: StateFlow<MainUiState> = MutableStateFlow(MainUiState.InitialState)
+    val uiState: Flow<MainUiState> = _uiState
 
     init {
         mainInteractor.checkParameters()
