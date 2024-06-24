b316af4998b4b976c3a1e8098dbd696e32f5b4ba
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-24 19:18:25 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/AddSpendingScreen.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/AddSpendingScreen.kt
@@ -1,17 +1,33 @@
 package evgenii.goncharov.econome.spending_impl.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import evgenii.goncharov.econome.spending_impl.view.models.AddSpendingViewModel
 
 @Composable
 internal fun AddSpendingScreen(viewMode: AddSpendingViewModel) {
-    Text(
-        text = "3. AddSpendingDialogFragment Диалоговое окно добавления расхода",
-        color = Color.Black,
-        fontSize = 20.sp
+    AddSpendingContent(
+        modifier = Modifier.height(400.dp)
     )
 }
 
+@Composable
+private fun AddSpendingContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "3. AddSpendingDialogFragment Диалоговое окно добавления расхода",
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+    }
+}
