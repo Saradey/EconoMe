16efe548fe29e272039da3cc55cc8ea91096075f
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-24 19:32:49 +0300

    added code

---
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/models/AddSpendingUiState.kt
@@ -0,0 +1,5 @@
+package evgenii.goncharov.econome.spending_impl.models
+
+internal data class AddSpendingUiState(
+    val inputSpending: String = ""
+)
--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
@@ -1,9 +1,16 @@
 package evgenii.goncharov.econome.spending_impl.view.models
 
 import androidx.lifecycle.ViewModel
+import evgenii.goncharov.econome.spending_impl.models.AddSpendingUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-internal class AddSpendingViewModel @Inject constructor(): ViewModel() {
+internal class AddSpendingViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState: MutableStateFlow<AddSpendingUiState> =
+        MutableStateFlow(AddSpendingUiState())
+    val uiState: StateFlow<AddSpendingUiState> = _uiState
 
     fun inputSpending(spending: String) {
 
