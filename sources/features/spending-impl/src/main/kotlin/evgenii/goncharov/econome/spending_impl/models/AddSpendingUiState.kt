c35440564e826e7895b539edb7d9a1de47b90884
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-27 08:33:41 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/models/AddSpendingUiState.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/models/AddSpendingUiState.kt
@@ -3,4 +3,5 @@ package evgenii.goncharov.econome.spending_impl.models
 internal data class AddSpendingUiState(
     val inputSpending: String = "",
     val inputComment: String = "",
+    val spendingCategories: List<SpendingCategory> = emptyList()
 )
