32a615c970724df21cf9d7bce0c5f67aab4abeea
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-27 08:07:04 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
@@ -13,6 +13,8 @@ internal class AddSpendingViewModel @Inject constructor() : ViewModel() {
     val uiState: StateFlow<AddSpendingUiState> = _uiState
 
     fun inputSpending(spending: String) {
-
+        _uiState.value = _uiState.value.copy(
+            inputSpending = spending
+        )
     }
 }
