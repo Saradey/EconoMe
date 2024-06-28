c5eba80de76beb34ce6290587e88ee61ffa7a308
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-06-28 23:35:24 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/fragments/AddSpendingDialogFragment.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/fragments/AddSpendingDialogFragment.kt
@@ -1,12 +1,17 @@
 package evgenii.goncharov.econome.spending_impl.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.compose.runtime.Composable
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import evgenii.goncharov.econome.common.ui.SystemEvent
 import evgenii.goncharov.econome.di_core.CoreBottomSheetFragment
 import evgenii.goncharov.econome.spending_api.di.AddSpendingApi
 import evgenii.goncharov.econome.spending_impl.di.contracts.AddSpendingInternal
 import evgenii.goncharov.econome.spending_impl.ui.AddSpendingScreen
 import evgenii.goncharov.econome.spending_impl.view.models.AddSpendingViewModel
+import kotlinx.coroutines.launch
 
 /**
  * 3. Screen
@@ -20,11 +25,23 @@ internal class AddSpendingDialogFragment : CoreBottomSheetFragment() {
         dependency.provideViewModelFactory()
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch { viewModel.systemEvent.collect { event -> handleSystemEvent(event) } }
+    }
+
     @Composable
     override fun InitContent() = AddSpendingScreen(viewModel)
 
     override fun releaseDependencies() {
         releaseFeatureApi(AddSpendingApi::class.java)
+    }
+
+    private fun handleSystemEvent(event: SystemEvent) {
+        when (event) {
+            SystemEvent.DismissDialog -> dismiss()
+            else -> Unit
+        }
     }
 
     companion object {
