4a00d2872e37c52615bebb78c76a825f0c350dfc
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-07-04 17:52:25 +0300

    added code

--- sources/engine/di-core/src/main/kotlin/evgenii/goncharov/econome/di_core/contracts/ReleasableApi.kt
+++ sources/engine/di-core/src/main/kotlin/evgenii/goncharov/econome/di_core/contracts/ReleasableApi.kt
@@ -1,8 +1,10 @@
 package evgenii.goncharov.econome.di_core.contracts
 
 import androidx.lifecycle.ViewModelProvider
+import evgenii.goncharov.econome.di_core.di.qualifiers.CommonViewModelFactory
 
 public interface ReleasableApi {
 
+    @CommonViewModelFactory
     public fun provideViewModelFactory(): ViewModelProvider.Factory
 }
---
+++ sources/engine/di-core/src/main/kotlin/evgenii/goncharov/econome/di_core/di/qualifiers/CommonViewModelFactory.kt
@@ -0,0 +1,7 @@
+package evgenii.goncharov.econome.di_core.di.qualifiers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+public annotation class CommonViewModelFactory
