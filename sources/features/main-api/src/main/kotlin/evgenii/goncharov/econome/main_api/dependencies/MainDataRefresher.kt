52993618c061c861fcfcb0c60daf8b85f77b9ab8
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-07-04 13:54:40 +0300

    added code

---
+++ sources/features/main-api/src/main/kotlin/evgenii/goncharov/econome/main_api/dependencies/MainDataRefresher.kt
@@ -0,0 +1,6 @@
+package evgenii.goncharov.econome.main_api.dependencies
+
+public interface MainDataRefresher {
+
+    public fun refreshData()
+}
---
+++ sources/features/main-impl/src/main/kotlin/evgenii/goncharov/econome/main_impl/dependencies/MainDataRefresherImpl.kt
@@ -0,0 +1,10 @@
+package evgenii.goncharov.econome.main_impl.dependencies
+
+import evgenii.goncharov.econome.main_api.dependencies.MainDataRefresher
+
+public class MainDataRefresherImpl : MainDataRefresher {
+
+    override fun refreshData() {
+
+    }
+}
