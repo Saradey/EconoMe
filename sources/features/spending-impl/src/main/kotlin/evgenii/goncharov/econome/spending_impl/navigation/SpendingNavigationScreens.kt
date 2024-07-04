35d583a82787814a842a840a1caf765f694f740a
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-07-04 17:41:06 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/navigation/SpendingNavigationScreens.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/navigation/SpendingNavigationScreens.kt
@@ -1,9 +1,6 @@
 package evgenii.goncharov.econome.spending_impl.navigation
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
 import com.github.terrakok.cicerone.androidx.FragmentScreen
-import evgenii.goncharov.econome.navigation.navigation.BACKSTACK_NAME_EVERYWHERE
 import evgenii.goncharov.econome.spending_impl.fragments.AddSpendingDialogFragment
 import evgenii.goncharov.econome.spending_impl.fragments.SpendingInfoFragment
 
@@ -13,10 +10,7 @@ internal object SpendingNavigationScreens {
         AddSpendingDialogFragment.newInstance()
     }
 
-    fun startDeepSpendingInfo(spendingId: Long) = object : FragmentScreen {
-        override val screenKey: String = BACKSTACK_NAME_EVERYWHERE
-        override fun createFragment(factory: FragmentFactory): Fragment {
-            return SpendingInfoFragment.newInstance(spendingId)
-        }
+    fun startDeepSpendingInfo(spendingId: Long) = FragmentScreen {
+        SpendingInfoFragment.newInstance(spendingId)
     }
 }
