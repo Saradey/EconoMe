9182e8b8047317beeeb3a6b1645c936579e87143
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-07-04 14:55:10 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/view/models/AddSpendingViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import evgenii.goncharov.econome.category.interactors.DefaultCategoryInteractor
 import evgenii.goncharov.econome.common.ui.SystemEvent
 import evgenii.goncharov.econome.current_user.repositories.CurrentWalletRepository
+import evgenii.goncharov.econome.main_api.dependencies.MainDataRefresher
 import evgenii.goncharov.econome.spending_impl.interactors.AddSpendingInteractor
 import evgenii.goncharov.econome.spending_impl.models.AddSpendingUiState
 import evgenii.goncharov.econome.spending_impl.models.SpendingModel
@@ -22,7 +23,8 @@ internal class AddSpendingViewModel @Inject constructor(
     mapper: MapperCategoryModelToSpendingCategory,
     private val inputSpendingValidatorUseCase: InputSpendingValidatorUseCase,
     private val addSpendingInteractor: AddSpendingInteractor,
-    private val currentWalletRepository: CurrentWalletRepository
+    private val currentWalletRepository: CurrentWalletRepository,
+    private val mainDataRefresher: MainDataRefresher
 ) : ViewModel() {
 
     private val _uiState: MutableStateFlow<AddSpendingUiState> =
@@ -91,6 +93,7 @@ internal class AddSpendingViewModel @Inject constructor(
                         createAt = Calendar.getInstance().time
                     )
                 )
+                mainDataRefresher.refreshListener?.invoke()
                 _systemEvent.value = SystemEvent.DismissDialog
             }
         }
