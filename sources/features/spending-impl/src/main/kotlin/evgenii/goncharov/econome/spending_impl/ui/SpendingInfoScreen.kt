251e73cfa8a22e06d6868d6738d08b8eced5ccc5
Evgenii Goncharov <evgenii.goncharov@indriver.com>
2024-07-04 22:27:28 +0300

    added code

--- sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/SpendingInfoScreen.kt
+++ sources/features/spending-impl/src/main/kotlin/evgenii/goncharov/econome/spending_impl/ui/SpendingInfoScreen.kt
@@ -1,11 +1,18 @@
 package evgenii.goncharov.econome.spending_impl.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import evgenii.goncharov.econome.spending_impl.models.SpendingInfoModel
 import evgenii.goncharov.econome.spending_impl.models.SpendingInfoUiState
@@ -28,8 +35,14 @@ private fun SpendingInfoContent(
     spendingInfoModel: SpendingInfoModel
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     ) {
-
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "5. SpendingInfoBottomSheetFragment Диалоговое информации по расходу",
+            color = Color.Black,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
